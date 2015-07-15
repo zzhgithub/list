b655352b422c6ed020650ccb27a7bb6ee38b817b
zzhgithub <1042181618@qq.com>
2015-07-15 16:32:05 +0800

    finish

--- list.php
+++ list.php
@@ -1,10 +1,41 @@
 <?php
 if(isset($_POST['submit'])){
-	$url = $_POST['url'];
-	$content = simplexml_load_file($url);
-	$name = (string)$content->name;
-	var_dump($name);
+	getfiles($_POST['url']);
+	//$str = "isi.xml";
+	// if(preg_match("/.xml/",$str)){
+ //    echo '成功';
+	// }else{
+ //    echo '没有内容';
+//	}
 }
+	function dealxml($url)
+	{
+		$content = simplexml_load_file($url);
+		$name = (string)$content->application->name;
+		return $name;
+	}
+	
+///////////////////////////////////////////////
+	function getfiles($path){ 
+	foreach(scandir($path) as $afile)
+	{
+		if($afile=='.'||$afile=='..') continue; 
+		if(is_dir($path.'/'.$afile)) 
+		{ 
+			getfiles($path.'/'.$afile); 
+		} else { 
+		if(preg_match('/manifest.xml/',$afile)){
+		  echo "<meta charset='utf8'>".$path.'--->'.dealxml($path.'/'.$afile).'<br />';
+			}else{
+
+			}
+		//echo $path.'/'.$afile.'<br />'; 
+		//echo $afile.'<br />';
+		      } 
+		} 
+	} 
+	
+
 ?>
 <!doctype html>
 <html>
